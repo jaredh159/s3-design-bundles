a4bc6141bd92b3131cac973bf85dd3f6a60fd41c
Brian Scaturro <scaturrob@gmail.com>
2016-03-30 14:52:43 -0400

    Initial commit

---
+++ s3-design-bundles.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package S3 Design Bundles
+ * @version 1.0.0
+ */
+/*
+Plugin Name: S3 Design Bundles
+Plugin URI: http://prophoto.com
+Description: Supports bundling designs to amazon s3
+Author: Brian Scaturro
+Version: 1.0.0
+Author URI: http://github.com/brianium
+*/
+require 'vendor/autoload.php';
+?>
