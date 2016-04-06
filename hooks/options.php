9c1906903635b6da41be3314d5974945b7c3cc58
Brian Scaturro <scaturrob@gmail.com>
2016-04-06 13:29:13 -0400

    Uses site options

--- hooks/options.php
+++ hooks/options.php
@@ -4,9 +4,9 @@ add_action('init', function () {
     $options = ['bucket', 'region', 'key', 'secret'];
     foreach ($options as $option) {
         $key = "prophoto_s3_bundler_$option";
-        $current = get_option($key);
+        $current = get_site_option($key);
         if ($current === false) {
-            update_option($key, '', false);
+            update_site_option($key, '', false);
         }
     }
 });
