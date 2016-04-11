635e63826d9725580424d092d33761bf135b3f4d
Jared Henderson <jared@netrivet.com>
2016-04-11 15:33:18 -0400

    fix fqcn for designer plugin container binding

--- hooks/container.php
+++ hooks/container.php
@@ -6,7 +6,7 @@ add_action('pp_container_binding', function (Container $container) {
     require_once __DIR__ . '/../src/Design/S3Bundler.php';
 
     $container
-        ->when('ProPhoto\DesignerPlugin\Middleware\ApiMiddleware')
+        ->when('ProPhoto\DesignerPlugin\Middleware\ApiMiddleware\DesignStoreApiMiddleware')
         ->needs('ProPhoto\Core\Service\Design\BundlerInterface')
         ->give('ProPhoto\S3DesignBundles\Design\S3Bundler');
 });
